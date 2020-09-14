0bd34edcd968094bde47e6baf8cc5251838749a1
code36u4r60 <code36u4r60@hotmail.com>
2020-09-14 17:07:16 +0100

    Project created

---
+++ backend/ranksearch/src/main/java/com/code36u4r60/ranksearch/RanksearchApplication.java
@@ -0,0 +1,13 @@
+package com.code36u4r60.ranksearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanksearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanksearchApplication.class, args);
+	}
+
+}
---
+++ backend/ranksearch/src/test/java/com/code36u4r60/ranksearch/RanksearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code36u4r60.ranksearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanksearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
