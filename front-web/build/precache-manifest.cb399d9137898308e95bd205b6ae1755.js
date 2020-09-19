bf48c4ff1ac657eefa5b84179761822b95b3eebf
Eduardo Queirós <al22653@utad.eu>
2020-09-19 22:35:00 +0100

    Add files via upload

---
+++ front-web/build/precache-manifest.cb399d9137898308e95bd205b6ae1755.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166733f8f31e6d2d1b2f45f862f7f370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b109825e4497f98470",
+    "url": "/static/css/main.822bcfe1.chunk.css"
+  },
+  {
+    "revision": "06b06494f5833662deb7",
+    "url": "/static/js/2.a4c7d028.chunk.js"
+  },
+  {
+    "revision": "54815be02e01897f642aa6f239b15dec",
+    "url": "/static/js/2.a4c7d028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b109825e4497f98470",
+    "url": "/static/js/main.2eb1bbdc.chunk.js"
+  },
+  {
+    "revision": "b63f7e6cff57c9ac9ac6",
+    "url": "/static/js/runtime-main.6d8a8fcb.js"
+  },
+  {
+    "revision": "c08814321789f1d603b3a5cc2de108a5",
+    "url": "/static/media/arrow.c0881432.svg"
+  },
+  {
+    "revision": "8d5d159ed7f4dddb22c59da9958c01e3",
+    "url": "/static/media/gamer.8d5d159e.svg"
+  },
+  {
+    "revision": "efb918734459ad437c0e3127c2f97235",
+    "url": "/static/media/logo.efb91873.svg"
+  }
+]);
